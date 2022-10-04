f18a5357a065b3d56fd620b44d93db086acbd9a2
cristicazan <a@b.c>
2022-10-04 16:28:47 +0300

    Added service bus support

--- Azure Workshop/Controllers/BankAccountController.cs
+++ Azure Workshop/Controllers/BankAccountController.cs
@@ -1,9 +1,11 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Azure_Workshop.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class BankAccountController : ControllerBase
     {
         private static readonly string[] names = new[]
@@ -22,17 +24,51 @@ namespace Azure_Workshop.Controllers
             "Kathy",
         };
 
-        public BankAccountController() { }
+        private readonly ServiceBusClient _serviceBusClient;
 
-        [HttpGet(Name = "GetTransactions")]
+        public BankAccountController(ServiceBusClient serviceBusClient)
+        {
+            _serviceBusClient = serviceBusClient;
+        }
+
+        [HttpGet]
         public IEnumerable<Transaction> GetTransactions()
         {
-            return Enumerable.Range(1, 10).Select(index => new Transaction
+            return Enumerable.Range(1, 10).Select(GetRandomTransaction).ToArray();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendMessageTransaction()
+        {
+            var message = new ServiceBusMessage(JsonSerializer.Serialize(GetRandomTransaction()));
+
+            var sender = _serviceBusClient.CreateSender("transactions");
+
+            await sender.SendMessageAsync(message);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<Transaction?> GetMessageTransaction()
+        {
+            var receiver  = _serviceBusClient.CreateReceiver("transactions");
+
+            var message = await receiver.ReceiveMessageAsync();
+
+            await receiver.CompleteMessageAsync(message);
+
+            return JsonSerializer.Deserialize<Transaction>(message.Body.ToString());
+        }
+
+        private Transaction GetRandomTransaction(int index = 1)
+        {
+            return new Transaction
             {
                 Date = DateTime.Now.AddDays(index),
                 Value = Random.Shared.Next(-20, 55),
                 Name = names[Random.Shared.Next(names.Length)]
-            }).ToArray();
+            };
         }
     }
 }
--- Azure Workshop/Program.cs
+++ Azure Workshop/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Azure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAzureClients(clientsBuilder =>
+{
+    clientsBuilder.AddServiceBusClient(builder.Configuration.GetConnectionString("ServiceBus"));
+});
 
 var app = builder.Build();
 

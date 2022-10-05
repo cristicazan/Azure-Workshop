12dd5fa0528aae2681617bb5434980af36c8101c
cristicazan <a@b.c>
2022-10-05 14:12:02 +0300

    Add support for key vault in function

--- Background Processor/Startup.cs
+++ Background Processor/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: FunctionsStartup(typeof(Background_Processor.Startup))]
 namespace Background_Processor
@@ -15,11 +18,21 @@ namespace Background_Processor
 
             builder.Services.AddAzureClients(clientsBuilder =>
             {
-                clientsBuilder.AddServiceBusClient(configuration.GetConnectionString("ServiceBus"));
+                clientsBuilder.AddServiceBusClient(configuration["ServiceBus"]);
             });
 
             builder.Services.AddDbContext<BackgroundProcessorDbContext>(
                 options => options.UseSqlServer(configuration.GetConnectionString("SQL")));
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            var configuration = builder.ConfigurationBuilder.Build();
+
+            builder.ConfigurationBuilder.AddAzureKeyVault(
+                new Uri($"https://{configuration["KeyVaultName"]}.vault.azure.net/"),
+                new DefaultAzureCredential(),
+                new AzureKeyVaultConfigurationOptions());
         }
     }
 }
